39577e78f94695b8da7153907ea8e294231d36ad
gyf304 <gyf304@users.noreply.github.com>
2017-01-23 12:35:50 -0500

    Update cc0.rb
    Cleaning up code

--- Formula/cc0.rb
+++ Formula/cc0.rb
@@ -20,10 +20,6 @@ class Cc0 < Formula
     rm "bin/codex"
     libexec.install %w[bin include runtime lib c0-mode]
     doc.install Dir["doc/*"]
-    # bin.install_symlink libexec/"bin/cc0.bin" => "cc0"
-    # bin.install_symlink libexec/"bin/coin.bin" => "coin"
-    # bin.install_symlink libexec/"bin/coin-exec.bin" => "coin-exec"
-    # bin.install_symlink libexec/"bin/codex.bin" => "codex"
     (bin/"cc0").write <<-EOS.undent
       #!/bin/sh
       #{libexec}/bin/cc0.bin $*
@@ -46,15 +42,6 @@ class Cc0 < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test cc0-v`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "#{bin}/cc0" "-h"
+    system "#{bin}/cc0", "-h"
   end
 end
